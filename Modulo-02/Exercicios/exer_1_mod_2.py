218028ff52234e33d7c751b7aaff68353ef4a812
Caio André <caio_andre15@hotmail.com>
2022-02-09 07:07:00 -0300

    Adicionando modulo 02 e o exercício 01

---
+++ Modulo-02/Exercicios/exer_1_mod_2.py
@@ -0,0 +1,11 @@
+# Exercicio 1 modulo 2
+
+textoSimples = "texto"
+numeroInteiro = 1
+numeroDecimal = 10.2
+valorVerdadeiro = True
+
+print(type(textoSimples))
+print(type(numeroInteiro))
+print(type(numeroDecimal))
+print(type(valorVerdadeiro))
