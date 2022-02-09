4f24e31a70c5cabef6a6ee0399657b9221404803
Caio André <caio_andre15@hotmail.com>
2022-02-09 07:24:33 -0300

    Adicionando ao modulo 02 o exercício 02

---
+++ Modulo-02/Exercicios/exer_2_mod_2.py
@@ -0,0 +1,9 @@
+# Exercicio 1 modulo 2
+valor_compras = 35.2
+desconto = 5.0
+quantidade_itens = 3
+
+# Valor final das compras
+print(round((valor_compras - desconto), 2))
+# Custo médio de cada item
+print(round((valor_compras / quantidade_itens), 2))
