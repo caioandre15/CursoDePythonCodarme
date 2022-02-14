663c3261f6776a4f5f513e6460c3289b00df4dbe
Caio André <caio_andre15@hotmail.com>
2022-02-14 07:09:02 -0300

    Adicionando exer. 01 repeticao

---
+++ Modulo-05/Exercicios/Repeticao/exer_01.py
@@ -0,0 +1,11 @@
+# Exer. 01 Soma de todos os números
+
+numeroInteiro = int(input("Digite um número inteiro: "))
+soma = 1
+i = 2
+while i <= numeroInteiro:
+    soma += i
+    i += 1
+    
+print(soma)
+
