f8baccd2a0c5c74eb7d6b0d9fe6c89413cde4434
Caio André <caio_andre15@hotmail.com>
2022-02-15 20:51:40 -0300

    Adicionando aula 02 - Tuplas

---
+++ Modulo-06/Aulas/aula_02.py
@@ -0,0 +1,27 @@
+pessoa = ("Gabriel", 27)
+
+turma = (
+    ("Nono Ano", 3),
+    [8, 9, 7],
+)
+
+# Ex. Com listas
+# notas = [8, 10]
+# print(notas)
+# notas[0] = 9
+
+# Ex. Com tuplas - São imutáveis
+# notas = (8, 10)
+# print(notas)
+# notas[0] = 9
+
+pessoa = ("Gabriel", 27, True)
+
+nome, idade, admin = pessoa # Desempacotamento de tuplas
+
+print(nome, idade, admin)
+
+pessoas = [
+    ("Gabriel", 27, True),
+    ("Bruno", 30, True)
+]
