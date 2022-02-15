2305b505eadd70aedd4af2c621a31858f9ef7200
Caio André <caio_andre15@hotmail.com>
2022-02-15 06:32:21 -0300

    Adicionando exer. 04 - Repetição

---
+++ Modulo-05/Exercicios/Repeticao/exer_04.py
@@ -0,0 +1,24 @@
+import os
+
+# Exer. 04 - O Jogo
+
+numero = 8
+
+resp = "s"
+while resp == "s":
+    os.system('cls') or None
+    print("### Jogo de adivinhação - Acerte o Número ###")
+    print("Em qual número inteiro estou pensando?...")
+    i = 1
+    while i <= 3:
+        tentativa = int(input(f"Tentativa número {i}: "))
+        if tentativa > numero:
+            print("O número digitado é maior do que o número secreto.")
+            i += 1
+        elif tentativa < numero:
+            print("O número digitado é menor do que o número secreto..")
+            i += 1
+        elif tentativa == numero:
+            print("Você acertou o número secreto, parabéns!!")
+            break
+    resp = input("Deseja jogar novamente? (s) sim (n) não: ")
