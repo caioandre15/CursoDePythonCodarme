c0acfce621ffc6c8dffc95f3e0f880bbf2a9d2b2
Caio André <caio_andre15@hotmail.com>
2022-02-17 20:39:26 -0300

    Adicionando aula 05

---
+++ Modulo-06/Aulas/aula_05.py
@@ -0,0 +1,29 @@
+# Percorrendo Estruturas de dados
+# for in
+
+notas = [8, 9, 10]
+
+for nota in notas:
+    print(nota)
+
+pessoas = {
+    "alice": 10,
+    "bob": 8,
+    "carlos": 9
+}
+
+# dict.keys()
+# dict.values()
+# dict.items()
+
+# for k, v in pessoas.items():
+#     print(k, v)
+
+for pessoa in pessoas.values():
+    print(pessoa)
+
+# for pessoa in pessoas.values():
+#     print(pessoa)
+
+# for pessoa in pessoas.keys:
+#     print(pessoa)
