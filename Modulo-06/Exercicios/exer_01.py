7aec3ccf8a8341e7f65e5a2385bad4ce1e116da5
Caio André <caio_andre15@hotmail.com>
2022-02-17 21:13:26 -0300

    Adicionando exercicio 01

---
+++ Modulo-06/Exercicios/exer_01.py
@@ -0,0 +1,12 @@
+# Exer. 01 Lê números positivos e adiciona em uma lista.
+
+numeroInteiro = 1
+listaDeNumerosInteiros = []
+while numeroInteiro > 0:
+    numeroInteiro = int(input("Digite um número inteiro positivo: "))
+    if numeroInteiro > 0:
+        listaDeNumerosInteiros.append(numeroInteiro)
+
+print(listaDeNumerosInteiros)
+    
+
