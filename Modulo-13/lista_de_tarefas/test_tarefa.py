03ba5fc206099e2637db55a976652550d6735e44
Caio André <caio_andre15@hotmail.com>
2022-03-22 23:01:31 -0300

    Adicionando teste adiar notificação

--- Modulo-13/lista_de_tarefas/test_tarefa.py
+++ Modulo-13/lista_de_tarefas/test_tarefa.py
@@ -1,5 +1,6 @@
 import unittest
 from tarefa import Tarefa
+from datetime import date, datetime
 
 
 class TestConcluir(unittest.TestCase):
@@ -21,4 +22,11 @@ class TestAdicionarDescricao(unittest.TestCase):
         tarefa.adicionar_descricao("Aulas ao vivo")
         self.assertNotEqual(tarefa.descricao, "")
 
+class TestAdiarNotificacao(unittest.TestCase):
+    def test_adia_notificacao_em_N_minutos(self):
+        dt_original = datetime(2022, 2, 10, 9, 10) # year, ,month, day, hour, minute, second, millisecond
+        tarefa = Tarefa("Estudar Python", data_notificacao=dt_original)
+        tarefa.adiar_notificacao(15)
+        dt_esperado = datetime(2022, 2, 10, 9, 25)
+        self.assertEqual(tarefa.data_notificacao, dt_esperado)
 unittest.main()
