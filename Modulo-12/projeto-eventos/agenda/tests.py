5bd8f18f820df22edde4799b776387816b7a0930
Caio André <caio_andre15@hotmail.com>
2022-03-29 22:50:05 -0300

    Acionando test automatizado

--- Modulo-12/projeto-eventos/agenda/tests.py
+++ Modulo-12/projeto-eventos/agenda/tests.py
@@ -1,4 +1,6 @@
 from django.test import TestCase, Client
+from agenda.models import Evento, Categoria
+from datetime import date
 
 class TestPaginaIncial(TestCase):
     def test_lista_eventos(self):
@@ -7,6 +9,27 @@ class TestPaginaIncial(TestCase):
         # print(response.content)
         # self.assertContains(response, "<th>Nome</th>")
         self.assertTemplateUsed(response, "agenda/listar_eventos.html")
-        
+
+class TestListagemDeEventos(TestCase):
+    def test_evento_com_data_de_hoje_e_exibido(self):
+        # Criando Categoria
+        categoria = Categoria()
+        categoria.name = "Backend"
+        categoria.save()
+        # Criando Evento
+        evento = Evento()
+        evento.nome = "Aula de Python"
+        evento.categoria = categoria
+        evento.local = "Rio de Janeiro"
+        evento.data = date.today()
+        evento.save()
+
+        client = Client()
+        response = client.get("/")
+        self.assertContains(response, "Aula de Python")
+        self.assertEqual(response.context["eventos"][0], evento)
+        self.assertEqual(list(response.context["eventos"]), [evento])
+
+
 
 
