cd4356a052ce6d56991d8c79aa51d8a79c2574f7
Caio André <caio_andre15@hotmail.com>
2022-04-05 01:15:35 -0300

    Adcionando rota de criacao de agendamento

--- Modulo-14/drf-api/agenda/views.py
+++ Modulo-14/drf-api/agenda/views.py
@@ -1,3 +1,4 @@
+import email
 from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
@@ -12,8 +13,23 @@ def agendamento_detail(request, id):
     serializer = AgendamentoSerializer(obj)
     return JsonResponse(serializer.data)
 
-@api_view(http_method_names=["GET"])
+@api_view(http_method_names=["GET", "POST"])
 def agendamento_list(request):
-    qs = Agendamento.objects.all()
-    serializer = AgendamentoSerializer(qs, many=True)
-    return JsonResponse(serializer.data, safe=False) 
+    if request.method == "GET":
+        qs = Agendamento.objects.all()
+        serializer = AgendamentoSerializer(qs, many=True)
+        return JsonResponse(serializer.data, safe=False) 
+    if request.method == "POST":
+        data = request.data # {"nome_cliente": "Gabriel"}...
+        serializer = AgendamentoSerializer(data=data)
+        if serializer.is_valid():
+            validated_data = serializer.validated_data
+            Agendamento.objects.create(
+                data_horario=validated_data["data_horario"],
+                nome_cliente=validated_data["nome_cliente"],
+                email_cliente=validated_data["email_cliente"],
+                telefone_cliente=validated_data["telefone_cliente"]
+            )
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+    
