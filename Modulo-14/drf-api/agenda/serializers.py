d6302b604ff556b78d06a266878341d36f5a17f2
Caio André <caio_andre15@hotmail.com>
2022-04-09 18:13:25 -0300

    Refatorando Create e Update

--- Modulo-14/drf-api/agenda/serializers.py
+++ Modulo-14/drf-api/agenda/serializers.py
@@ -1,4 +1,7 @@
+from django.utils import timezone
+from pytz import timezone
 from rest_framework import serializers
+from agenda.models import Agendamento
 
 
 class AgendamentoSerializer(serializers.Serializer):
@@ -7,3 +10,30 @@ class AgendamentoSerializer(serializers.Serializer):
     email_cliente = serializers.EmailField()
     telefone_cliente = serializers.CharField(max_length=20)
     # cancelado = serializers.BooleanField()
+
+    def validate_data_horario(self, value):
+        if value < timezone.now():
+            raise serializers.ValidationError("Agendamento não pode ser feito no passado!")
+        return value
+
+    def create(self, validated_data):
+        agendamento = Agendamento.instanceects.create(
+            data_horario=validated_data["data_horario"],
+            nome_cliente=validated_data["nome_cliente"],
+            email_cliente=validated_data["email_cliente"],
+            telefone_cliente=validated_data["telefone_cliente"]
+        )
+        return agendamento
+
+    def update(self, instance, validated_data):
+        instance.data_horario = validated_data.get(
+            "data_horario", instance.data_horario)
+        instance.nome_cliente = validated_data.get(
+            "nome_cliente", instance.nome_cliente)
+        instance.email_cliente = validated_data.get(
+            "email_cliente", instance.email_cliente)
+        instance.telefone_cliente = validated_data.get(
+            "telefone_cliente", instance.telefone_cliente)
+        # instance.cancelado = validated_data.get("cancelado", instance.cancelado)
+        instance.save()
+        return instance
--- Modulo-14/drf-api/agenda/views.py
+++ Modulo-14/drf-api/agenda/views.py
@@ -10,25 +10,17 @@ from rest_framework.response import Response
 
 @api_view(http_method_names=["GET", "PATCH", "DELETE"])
 def agendamento_detail(request, id):
+    obj = get_object_or_404(Agendamento, id=id)
     if request.method == "GET":
-        obj = get_object_or_404(Agendamento, id=id)
         serializer = AgendamentoSerializer(obj)
         return JsonResponse(serializer.data)
-    if request.method == "PATCH":
-        obj = get_object_or_404(Agendamento, id=id)
-        serializer = AgendamentoSerializer(data=request.data, partial=True)
+    if request.method == "PATCH":        
+        serializer = AgendamentoSerializer(obj, data=request.data, partial=True)
         if serializer.is_valid():
-            validated_date = serializer.validated_data
-            obj.data_horario = validated_date.get("data_horario", obj.data_horario)
-            obj.nome_cliente = validated_date.get("nome_cliente", obj.nome_cliente)
-            obj.email_cliente = validated_date.get("email_cliente", obj.email_cliente)
-            obj.telefone_cliente = validated_date.get("telefone_cliente", obj.telefone_cliente)
-            # obj.cancelado = validated_date.get("cancelado", obj.cancelado)
-            obj.save()
-            return JsonResponse(validated_date, status=200)
+            serializer.save()
+            return JsonResponse(serializer.data, status=200)
         return JsonResponse(serializer.errors, status=400)
     if request.method == "DELETE":
-        obj = get_object_or_404(Agendamento, id=id)
         obj.cancelado = True
         # obj.delete()
         return Response(status=204)
@@ -44,12 +36,6 @@ def agendamento_list(request):
         data = request.data  # {"nome_cliente": "Gabriel"}...
         serializer = AgendamentoSerializer(data=data)
         if serializer.is_valid():
-            validated_data = serializer.validated_data
-            Agendamento.objects.create(
-                data_horario=validated_data["data_horario"],
-                nome_cliente=validated_data["nome_cliente"],
-                email_cliente=validated_data["email_cliente"],
-                telefone_cliente=validated_data["telefone_cliente"]
-            )
+            serializer.save()
             return JsonResponse(serializer.data, status=201)
         return JsonResponse(serializer.errors, status=400)
